f5a094e17d0c8b88601e7cf48410b329d7c8b7b2
NevsCode <nevillempiana0328@gmail.com>
2025-12-25 01:03:19 +0200

    filling in the username ad password and signing in

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -9,6 +9,11 @@ const{test, expect} = require('@playwright/test');
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://rahulshettyacademy.com/loginpagePractise');
+    console.log( await page.title());
+
+    await page.locator("#username").fill('nevil');
+    await page.locator("#password").fill('nevil123');
+    await page.locator("#signInBtn").click();
     
     //get title
     
