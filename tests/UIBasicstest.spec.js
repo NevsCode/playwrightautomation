9f1927a1278ceeaad7273dffb648163028aa96f8
NevsCode <nevillempiana0328@gmail.com>
2025-12-24 13:26:02 +0200

    changing the url back to raulshettyacademy

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -8,7 +8,7 @@ const{test, expect} = require('@playwright/test');
 
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto('https://google.com');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise');
     
     //get title
     
