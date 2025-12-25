18206941750e9e836f907d5762c83a5027616baf
NevsCode <nevillempiana0328@gmail.com>
2025-12-25 02:50:03 +0200

    headless off

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',   // browser
-    headless: false,   // headed mode
+     
   },
 
 });
