b82e3de8c1bc3a7d2e7745998d2dea95ec08a965
NevsCode <nevillempiana0328@gmail.com>
2025-12-25 02:22:50 +0200

    declaration, clear element ,sign in and getting array of elements

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -7,16 +7,37 @@ const{test, expect} = require('@playwright/test');
   {
 
     const context = await browser.newContext();
+
     const page = await context.newPage();
     await page.goto('https://rahulshettyacademy.com/loginpagePractise');
+
+    const userName = page.locator("#username"); //locator element declaration
+    const password = page.locator("#password");
+    const signInBtn = page.locator("#signInBtn");
+    const cardTitles = page.locator(".card-body a");
+    
     console.log( await page.title());
 
-    await page.locator("#username").fill('nevil');
-    await page.locator("#password").fill('nevil123');
-    await page.locator("#signInBtn").click();
+    await userName.fill('nevil');  // typing on a element
+    await password.fill('nevil123'); // typing on a element  
     
-    //get title
+    await signInBtn.click(); // click on a element 
+
+    console.log(await page.locator("[style*='block']").textContent()); 
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect"); //get text - assertion
     
+    await userName.fill("");  // clear the element
+    await userName.fill('rahulshettyacademy');  // typing on a element
+    await password.fill('learning'); // typing on a element
+    await signInBtn.click(); // click on a element
+    
+    //console.log(await page.locator(".card-body a").nth(0).textContent()); //getting multiple elements - text b
+    console.log(await cardTitles.first().textContent()); //getting multiple elements - text a
+    const allTitles = await cardTitles.allTextContents();
+    [] //getting multiple elements - text
+    console.log(allTitles); //print array of texts
+
+
   });
 
 
